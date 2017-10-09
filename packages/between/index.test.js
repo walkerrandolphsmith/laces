3752f841ecc54aff2bcbab784fb910623e94a6ee
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-10-09 17:43:01 -0400

    rename between to isBetween

--- packages/between/index.test.js
+++ packages/between/index.test.js
@@ -1,19 +1,19 @@
 import test from 'ava';
-import between from './index';
+import isBetween from './index';
 
 const left = '<a>';
 const right = '</a>';
 const match = 'val';
 const string = `${left}${match}${right}`;
 
-test('Value between two patterns', t => t.true(between(string, left, right) === match));
+test('Value isBetween two patterns', t => t.true(isBetween(string, left, right) === match));
 
-test('When left pattern doesnt match return string', t => t.true(between(string, '<!a>', right) === string));
+test('When left pattern doesnt match return string', t => t.true(isBetween(string, '<!a>', right) === string));
 
-test('When right pattern doesnt match return string', t => t.true(between(string, left, '</!a>') === string));
+test('When right pattern doesnt match return string', t => t.true(isBetween(string, left, '</!a>') === string));
 
-test('Nested pattern', t => t.true(between(string + right, left, right) === match));
+test('Nested pattern', t => t.true(isBetween(string + right, left, right) === match));
 
-test('Treat "" as string start', t => t.true(between(string, '', right) === left + match));
+test('Treat "" as string start', t => t.true(isBetween(string, '', right) === left + match));
 
-test('Treat no right pattern as string end', t => t.true(between(string, left) === match + right));
+test('Treat no right pattern as string end', t => t.true(isBetween(string, left) === match + right));
