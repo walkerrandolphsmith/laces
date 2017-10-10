ef13e05076a632068445f2b3d62f366b9c17fcd8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-10-10 18:18:25 -0400

    trim is the combination of trimLeft and trimRight

--- packages/trim/index.js
+++ packages/trim/index.js
@@ -1,6 +1,4 @@
-//import trimLeft from '@quillio/trimLeft';
-//import trimRight from '@quillio/trimRight';
+import trimLeft from '@quillio/trimLeft';
+import trimRight from '@quillio/trimRight';
 
-export default (string, pattern=' ') => string
-    .replace(new RegExp(`^\\${pattern}*`), '')
-    .replace(new RegExp(`\\${pattern}*$`), '')
+export default (string, pattern=' ') => trimRight(trimLeft(string, pattern), pattern);
